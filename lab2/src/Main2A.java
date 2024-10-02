65f3c4a12e7e40029626d49307987cbfe9e62b88
Sukhbat A - laptop <ssuheesh@gmail.com>
2024-10-01 19:54:53 -0500

    fixed

--- lab2/src/Main2A.java
+++ lab2/src/Main2A.java
@@ -2,7 +2,6 @@ import prob2A.Student;
 
 public class Main2A {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         Student s1 = new Student("Sukhbat", "4.0");
         Student s2 = new Student("Bob", "3.2");
         Student s3 = new Student("John", "2.9");
